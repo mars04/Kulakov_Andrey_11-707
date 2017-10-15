074e8b670f959afa0c5d8d98ad579508e3361eaf
andrkulakoff <andrkulakoff@gmail.com>
2017-10-15 14:43:48 +0300

    Upload first part

--- PS/PS1/Part_1-14/Program.cs
+++ PS/PS1/Part_1-14/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Part_1_14
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+        int x = int.Parse(Console.ReadLine());
+        int a, b, c, d, e, f, y, z;
+        a = x / 100000;
+        x = x % 100000;
+        b = x / 10000;
+        x = x % 10000;
+        c = x / 1000;
+        x = x % 1000;
+        d = x / 100;
+        x = x % 100;
+        e = x / 10;
+        x = x % 10;
+        f = x ;
+        y = a + b + c;
+        z = d + e + f;
+        if (y == z)
+            Console.WriteLine("Счастливый");
+        else Console.WriteLine("Не счастливый");
+        Console.ReadLine();
         }
     }
 }
--- PS/PS1/Part_1-7/Program.cs
+++ PS/PS1/Part_1-7/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Part_1_7
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        if ((a + b) <= c)
+            Console.WriteLine("No");
+        else
+        if ((a + c) <= b)
+            Console.WriteLine("No");
+        else
+        if ((b + c) <= a)
+            Console.WriteLine("No");
+        else
+            Console.WriteLine("Yes");
+        Console.ReadLine();
+
         }
     }
 }
