b9ef5047a3a996b747bd03800e0f46a39ce78f4e
andrkulakoff <andrkulakoff@gmail.com>
2017-10-15 21:34:05 +0300

    gotovo

--- PS/PS1/Part_3-17/Program.cs
+++ PS/PS1/Part_3-17/Program.cs
@@ -8,10 +8,28 @@ namespace Part_3_17
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        
+        static void Main(string[] args) { 
+        Console.Write("Введите конец диапазона от 2 до : ");
+        int n = int.Parse(Console.ReadLine());
+            int k = 0;
+            int s = 0;
+        for(int i=2; i<=n; i++)
+            {
+        for(int j=2; j<i; j++)
+                {
+                    if (i % j == 0)
+                       k++;
+                    
+                }
+                if (k == 0)
+                    s++;
+                else k = 0;
+
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
         }
+  
        
     }
 }
