b34f2d88f5b408fc777fe07a66bc50953206eeab
andrkulakoff <andrkulakoff@gmail.com>
2017-10-15 14:51:07 +0300

    part 2

---
+++ PS/PS1/Part_2-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int f = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        int k;
+        //n = f + s · (k - 1) 
+        k = (n - f) / s + 1;
+        if ((n - f) % s != 0)
+            Console.WriteLine("-1");
+        else
+            Console.WriteLine(k);
+        Console.ReadLine();
+        }
+    }
+}
